78ea7f5e9f1fe948813aced0db93380b1cd0d98e
ENJ23 <eliasjarma9@gmail.com>
2024-11-01 23:57:26 -0300

    Inicializacion Proyecto

---
+++ src/main/java/ar/edu/unju/escmi/tp7/Tp06PooGrupo55Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.escmi.tp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp06PooGrupo55Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp06PooGrupo55Application.class, args);
+	}
+
+}
