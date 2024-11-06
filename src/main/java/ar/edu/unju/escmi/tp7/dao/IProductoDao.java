1fb4f590f6f8ed33ea9bbde4322341482b433cc5
JULI <PC@JULI>
2024-11-06 17:25:35 -0300

    Interfaz de Producto JulietaVillena

--- src/main/java/ar/edu/unju/escmi/tp7/dao/IProductoDao.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/IProductoDao.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.escmi.tp7.dao;
 
-public interface IProductoDao {
+import ar.edu.unju.escmi.tp7.dominio.Producto;
 
+public interface IProductoDao {
+	
+	public Producto ingresoDatos();
+	public void altaProducto(Producto producto);
+	public void eliminarProducto(Producto producto);
+	public Producto buscarProducto(Long id);
+	public void modificarPrecio(Producto producto, double precioNuevo);
 }
