4f2fc404ef41fdfa2d869328d42a7ad92601e629
MATIAS <PC@MATIAS>
2024-11-06 00:31:19 -0300

    Clase Cliente MatiasBrotons

--- src/main/java/ar/edu/unju/escmi/tp7/dominio/Cliente.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dominio/Cliente.java
@@ -1,5 +1,70 @@
 package ar.edu.unju.escmi.tp7.dominio;
 
-public class Cliente {
 
+public class Cliente {
+    private Long id; // Autoincremental
+    private String nombre;
+    private String apellido;
+    private String domicilio;
+    private int dni;
+    private boolean estado;
+
+    // Constructor
+    public Cliente(Long id, String nombre, String apellido, String domicilio, int dni, boolean estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.domicilio = domicilio;
+        this.dni = dni;
+        this.estado = estado;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
 }
