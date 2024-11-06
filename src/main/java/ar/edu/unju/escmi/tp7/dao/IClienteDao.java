138afdc9047bd92d8b9874752558a4989d081766
JULI <PC@JULI>
2024-11-06 17:23:49 -0300

    Interfas de Cliente JulietaVillena

--- src/main/java/ar/edu/unju/escmi/tp7/dao/IClienteDao.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/IClienteDao.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.escmi.tp7.dao;
 
-public interface IClienteDao {
+import ar.edu.unju.escmi.tp7.dominio.Cliente;
 
+public interface IClienteDao {
+	
+	public Cliente ingresoDatos();
+	public void guardarCliente(Cliente cliente);
+	public void eliminarCliente(Cliente cliente);
+	public void modificarCliente(Cliente cliente);
+	public void mostrarClientes();
+	public Cliente buscarCliente(long id);
 }
