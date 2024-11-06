0befb7e091be0479ec94913a06dd6904cb1213e2
Sergio <PC@Sertin468>
2024-11-06 17:36:47 -0300

    Continuacion del Main

--- src/main/java/ar/edu/unju/escmi/tp7/main/Main.java
+++ src/main/java/ar/edu/unju/escmi/tp7/main/Main.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.escmi.tp7.main;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import ar.edu.unju.escmi.tp7.dao.imp.ClienteDaoImp;
+import ar.edu.unju.escmi.tp7.dao.imp.FacturaDaoImp;
+import ar.edu.unju.escmi.tp7.dao.imp.ProductoDaoImp;
+import ar.edu.unju.escmi.tp7.dominio.Cliente;
+import ar.edu.unju.escmi.tp7.dominio.Factura;
+import ar.edu.unju.escmi.tp7.dominio.Producto;
+
+
 public class Main {
 
 }
