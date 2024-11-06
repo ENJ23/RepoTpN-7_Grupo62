49f16e8258a300746d3e7e02de2354edd16bf4ef
Cami <Cami@LAPTOP-GRDN8A38>
2024-11-06 16:18:03 -0300

    ClienteDaoImp métodos Tejeda Camila

--- src/main/java/ar/edu/unju/escmi/tp7/dao/imp/ClienteDaoImp.java
+++ src/main/java/ar/edu/unju/escmi/tp7/dao/imp/ClienteDaoImp.java
@@ -61,4 +61,116 @@ public class ClienteDaoImp implements IClienteDao {
 			//manager.close();
 		}
 	}
+	
+	@Override
+	public void eliminarCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		EntityTransaction transaction = manager.getTransaction();
+		try {
+			transaction.begin();
+			Cliente clienteAEliminar = manager.find(Cliente.class, cliente.getId());
+			clienteAEliminar.setEstado(false);
+			transaction.commit();
+		}catch (Exception e){
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			
+			System.out.println("No se pudo guardar el cliente. Intentelo nuevamente más tarde.");
+			System.out.println("Error: " + e.getMessage());
+		}finally {
+			//manager.close();
+		}
+	}
+
+	@Override
+	public void modificarCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		EntityTransaction transaction = manager.getTransaction();
+		try {
+			transaction.begin();
+			Cliente clienteAModificar = manager.find(Cliente.class, cliente.getId());
+
+			if (clienteAModificar != null) {	
+				
+				Cliente datosActualizados = ingresoDatos();
+				clienteAModificar.setNombre(datosActualizados.getNombre());
+	            clienteAModificar.setApellido(datosActualizados.getApellido());
+	            clienteAModificar.setDomicilio(datosActualizados.getDomicilio());
+	            clienteAModificar.setDni(datosActualizados.getDni());
+	            
+			transaction.commit();
+			
+			}else {
+				System.out.println("Cliente no encontrado");
+				transaction.rollback();
+			}
+		}catch (Exception e){
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			
+			System.out.println("No se pudo guardar el cliente. Intentelo nuevamente más tarde.");
+			System.out.println("Error: " + e.getMessage());
+		}finally {
+			//manager.close();
+		}
+	}
+
+	@Override
+	public void mostrarClientes() {
+		// TODO Auto-generated method stub
+        EntityTransaction transaction = manager.getTransaction();
+        try {
+        	transaction.begin();
+        	TypedQuery<Cliente> query = manager.createQuery("SELECT c FROM Cliente c", Cliente.class);
+        	List<Cliente> clientes = query.getResultList();
+        	if(clientes != null) {
+        	for (Cliente cliente : clientes) {
+                System.out.println("ID: " + cliente.getId() + ", Nombre: " + cliente.getNombre() +
+                                   ", Apellido: " + cliente.getApellido() +
+                                   ", Domicilio: " + cliente.getDomicilio() +
+                                   ", DNI: " + cliente.getDni() +
+                                   ", Estado: " + cliente.isEstado());
+            	}
+        	}else {
+        		System.out.println("No hay clientes registrados.");
+        		transaction.rollback();
+        	}
+        	transaction.commit();
+        }catch(Exception e) {
+        	if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			
+			System.out.println("No se pudo guardar el cliente. Intentelo nuevamente más tarde.");
+			System.out.println("Error: " + e.getMessage());
+        }finally {
+        	//manager.close();
+        }
+	}
+
+	@Override
+	public Cliente buscarCliente(long id) {
+			
+			EntityTransaction transaction = manager.getTransaction();
+	        Cliente cliente = null;
+
+	        try {
+	            transaction.begin();
+	            
+	            cliente = manager.find(Cliente.class, id);
+	            
+	            transaction.commit(); 
+	        } catch (Exception e) {
+	            if (transaction != null && transaction.isActive()) {
+	                transaction.rollback(); 
+	            }
+	            System.out.println("No se pudo buscar el cliente. Error: " + e.getMessage());
+	        } finally {
+	           // manager.close(); 
+	        }
+
+	        return cliente;
+	    }
 }
